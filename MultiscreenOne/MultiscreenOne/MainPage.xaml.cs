5c1ff4aa3188c6d1046609e031b78b05488a1612
Oscar Moreno <omorenomalaver@gmail.com>
2016-12-01 13:09:11 +0800

    first commit

---
+++ MultiscreenOne/MultiscreenOne/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MultiscreenOne
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Would you like to call " + translatedNumber + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    App.PhoneNumbers.Add(translatedNumber);
+                    callHistoryButton.IsEnabled = true;
+                    dialer.Dial(translatedNumber);
+                }
+            }
+
+        }
+
+        async void OnCallHistory(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CallHistoryPage());
+        }
+
+    }
+
+}
